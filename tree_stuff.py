34b346a9df12123df0e899b0c9806424667d9dff
Mario Arana <marioknaus22@gmail.com>
2025-03-27 11:43:33 +0100

    Add tree stuff

--- tree_stuff.py
+++ tree_stuff.py
@@ -1,7 +1,38 @@
 import numpy as np
 
+dictionary = {}
+
+
+def combinations(num, array):
+    if num == 0:
+        return 1
+
+    if num in dictionary:
+        return dictionary.get(num)
+
+    result = 0
+
+    for x in array:
+        if num - x >= 0:
+            combs = combinations(num - x, array)
+            result += combs
+
+    dictionary[num] = result
+
+    return result
+
+
+def min_comb(num, array):
+    min_indices = []
+
+
+
 def main():
-    pass
+    arr = np.array([1, 2, 3])
+    nums = np.array([3, 11, 15, 41])
+
+    for num in nums:
+        print(combinations(num, arr))
 
 
 if __name__ == "__main__":
